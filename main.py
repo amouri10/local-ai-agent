6bef96e0113a492fa58545002022272689794415
Omer Jaabri <158459242+amouri10@users.noreply.github.com>
2025-08-02 22:12:50 +0530

    Add files via upload
    This project is an AI-powered question answering system that helps users find the best pizza restaurants based on real customer reviews. It combines LangChain, Ollama LLMs, and a Chroma vector database to enable semantic search and natural language understanding.

---
+++ main.py
@@ -0,0 +1,30 @@
+from langchain_ollama.llms import OllamaLLM
+from langchain_core.prompts import ChatPromptTemplate
+from vector import retriever
+
+model = OllamaLLM(model="llama3.2")
+
+template = """ 
+You are an expert in answering questions about a pizza restaurant
+
+Here are some relevant reviews:
+{reviews}
+
+Here is the question to answer: {question} 
+"""
+
+prompt = ChatPromptTemplate.from_template(template)
+chain = prompt | model
+
+while True:
+    print("\n\n----------------------------------")
+    question = input("Ask your question (q to quit): ")
+    print("\n\n")
+    if question.lower() == "q":
+        break
+    
+    reviews = retriever.invoke(question)
+    review_texts = [doc.page_content for doc in reviews]
+
+    result = chain.invoke({"reviews": review_texts, "question": question})
+    print("Answer:", result)
